19e9017c46b374bab6fc4264d0856060c2d06673
vill <544912473@qq.com>
2018-12-20 14:14:30 +0800

    KeyanChu init

---
+++ KeyanChu_spider/spiders/KeyanChu_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+import mysql.connector
+import time
+import random
+import datetime
+import traceback
+
+"this is a spider for KeyanChu Mrs.Gao"
+
+
+class ZhaobiaoSpider(scrapy.Spider):
+    name = "KeyanChu"
+
+    def start_requests(self):
+
+
+    def parse(self, response):
+        pass
