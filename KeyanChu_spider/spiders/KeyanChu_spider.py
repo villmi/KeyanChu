ea6cdf20040848682b9b057d908c3759b486c0ea
vill <544912473@qq.com>
2018-12-20 14:20:09 +0800

    KeyanChu init

--- KeyanChu_spider/spiders/KeyanChu_spider.py
+++ KeyanChu_spider/spiders/KeyanChu_spider.py
@@ -12,7 +12,11 @@ class ZhaobiaoSpider(scrapy.Spider):
     name = "KeyanChu"
 
     def start_requests(self):
-
+        url = "http://keyan.njue.edu.cn/class.asp?ClassID=192&page=5"
+        yield scrapy.request(url=url, callback=self.parse)
+        pass
 
     def parse(self, response):
+        table = response.xpath('/html/body/table[4]/tbody/tr/td[2]/table[4]/tbody/tr/td[2]/table[1]')
+        print(table)
         pass
